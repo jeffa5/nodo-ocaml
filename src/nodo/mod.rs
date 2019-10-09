e7a00725a489767da98bcffec03da96469d0a4da
Jeffas <dev@jeffas.io>
2019-10-10 00:46:37 +0100

    Update Nodo struct
    Lists can now be mixed between text or tasks.
    
    Checkbox -> Task.
    
    filename and title are added. A filename is the actual name used to
    store the file in the fs. The title is the string used inside the nodo.
    
    Nodo is parameterised over the file reading and writing to bring in the
    read and write methods for this nodo. This allows each nodo to have
    it's own type of file handler.

--- src/nodo/mod.rs
+++ src/nodo/mod.rs
@@ -1,3 +1,5 @@
+use crate::files::NodoFile;
+use std::marker::PhantomData;
 /// A Nodo is a mixture of a todo and a note.
 ///
 /// They are formed of optional metadata and blocks.
@@ -10,23 +12,27 @@
 pub enum Block {
     /// A heading text with a level
     Heading(String, u32),
-    /// A sequence of elements, elements must be strings for now
-    List(Vec<String>),
-    /// A sequence of checkboxes
-    Checkboxes(Vec<Checkbox>),
+    /// A sequence of elements, elements can be text or tasks
+    List(Vec<ListItem>),
 }
 
 /// A checkbox represents a todo item which can be completed or not
 #[derive(Debug, PartialEq, Clone)]
-pub struct Checkbox {
-    text: String,
-    checked: bool,
+pub struct Task {
+    pub text: String,
+    pub checked: bool,
 }
 
-impl Checkbox {
-    pub fn new(text: String, checked: bool) -> Checkbox {
-        Checkbox { text, checked }
+impl Task {
+    pub fn new(text: String, checked: bool) -> Self {
+        Self { text, checked }
     }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ListItem {
+    Task(Task),
+    Text(String),
 }
 
 /// Metadata stores information about the nodo
@@ -34,61 +40,84 @@ impl Checkbox {
 pub struct Metadata {
     projects: Vec<String>,
     tags: Vec<String>,
+    title: String,
+    filename: String,
 }
 
 impl Metadata {
-    fn new() -> Metadata {
-        Metadata {
+    fn new() -> Self {
+        Self {
             projects: Vec::new(),
             tags: Vec::new(),
+            title: String::new(),
+            filename: String::new(),
         }
     }
 
     pub fn tags(&self) -> &[String] {
         &self.tags
     }
+
+    pub fn projects(&self) -> &[String] {
+        &self.projects
+    }
+
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn filename(&self) -> &str {
+        &self.filename
+    }
 }
 
 /// Nodos have explicit fields for their metadata e.g. project and tags
 /// Other content within the nodo is represented as a sequence of Blocks
 #[derive(Debug, PartialEq)]
-pub struct Nodo {
+pub struct Nodo<F: NodoFile> {
+    filetype: PhantomData<F>,
     /// The metadata associated with this nodo
     metadata: Metadata,
     /// The rest of the content
     blocks: Vec<Block>,
 }
 
-impl Nodo {
-    pub fn new() -> Nodo {
+impl<F: NodoFile> Nodo<F> {
+    pub fn new() -> Nodo<F> {
         Nodo {
+            filetype: PhantomData,
             metadata: Metadata::new(),
             blocks: Vec::new(),
         }
     }
 
-    pub fn project(&mut self, project: &str) -> &mut Self {
-        self.metadata.projects.push(project.to_owned());
+    pub fn projects(mut self, projects: &[String]) -> Self {
+        self.metadata.projects.append(&mut projects.to_vec());
         self
     }
 
-    pub fn tags(&mut self, tags: &[String]) -> &mut Self {
+    pub fn title(mut self, title: String) -> Self {
+        self.metadata.title = title;
+        self
+    }
+
+    pub fn filename(mut self, filename: String) -> Self {
+        self.metadata.filename = filename;
+        self
+    }
+
+    pub fn tags(mut self, tags: &[String]) -> Self {
         self.metadata.tags = tags.to_vec();
         self
     }
 
-    pub fn heading(&mut self, text: String, level: u32) -> &mut Self {
+    pub fn heading(mut self, text: String, level: u32) -> Self {
         self.blocks.push(Block::Heading(text, level));
         self
     }
 
-    pub fn list(&mut self, items: &[String]) -> &mut Self {
-        self.blocks.push(Block::List(items.to_vec()));
-        self
-    }
-
-    pub fn checkboxes(&mut self, items: &[Checkbox]) -> &mut Self {
-        self.blocks.push(Block::Checkboxes(items.to_vec()));
+    pub fn list(mut self, items: Vec<ListItem>) -> Self {
+        self.blocks.push(Block::List(items));
         self
     }
 
