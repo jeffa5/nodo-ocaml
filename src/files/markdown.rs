b30d39a0910d465508b2c8cf9964a581f031a1c7
Jeffas <dev@jeffas.io>
2019-10-10 00:40:16 +0100

    Update file interactions
    This updates the traits for reading and writing nodos. It also adjusts
    the trait to include an extension string and a way of getting the
    appropriate Nodo struct given the extension.

--- src/files/markdown.rs
+++ src/files/markdown.rs
@@ -1,8 +1,11 @@
-use crate::files::{NodoReader, NodoWriter, ReadError, WriteError};
-use crate::nodo::Nodo;
 use pulldown_cmark::{Event, Options, Parser, Tag};
+use std::io::{Read, Write};
 
-pub struct Markdown {}
+use crate::files::{NodoFile, ReadError, WriteError};
+use crate::nodo::{Block, ListItem, Nodo, Task};
+
+#[derive(Debug, PartialEq)]
+pub struct Markdown;
 
 struct EventsIter<'a> {
     events: Vec<Event<'a>>,
@@ -16,17 +19,21 @@ impl<'a> Iterator for &mut EventsIter<'a> {
         if self.index < self.events.len() {
             let item = self.events[self.index].clone();
             self.index += 1;
-            println!("{:?}", item);
+            if cfg!(test) {
+                eprintln!("Reading nodo element: {:?}", item);
+            }
             return Some(item);
         }
         None
     }
 }
 
-impl NodoReader for Markdown {
-    fn read<R: std::io::Read>(mut reader: R) -> Result<Nodo, ReadError> {
+impl NodoFile for Markdown {
+    const EXTENSION: &'static str = "md";
+
+    fn read<R: Read>(mut nodo: Nodo<Self>, reader: &mut R) -> Result<Nodo<Self>, ReadError> {
         let mut s = String::new();
-        reader.read_to_string(&mut s).unwrap();
+        reader.read_to_string(&mut s)?;
 
         let options = Options::all();
         let parser = Parser::new_ext(&s, options);
@@ -36,35 +43,67 @@ impl NodoReader for Markdown {
             index: 0,
         };
 
-        let mut nodo = Nodo::new();
+        nodo = read_frontmatter(nodo, &mut events_iter)?;
 
-        read_frontmatter(&mut nodo, &mut events_iter)?;
+        nodo = nodo.title(read_heading(&mut events_iter));
 
-        read_body(&mut nodo, &mut events_iter)?;
+        nodo = read_body(nodo, &mut events_iter)?;
 
         Ok(nodo)
     }
+
+    fn write<W: Write>(nodo: &Nodo<Self>, writer: &mut W) -> Result<(), WriteError> {
+        let mut metadata_lines = Vec::new();
+        let tag_string = nodo.metadata().tags().join(", ");
+        if tag_string != "" {
+            metadata_lines.push(format!("tags: {}", tag_string))
+        }
+        let metadata_str = metadata_lines.join("\n");
+        if metadata_str != "" {
+            writeln!(writer, "---")?;
+            writeln!(writer, "{}", &metadata_str)?;
+            writeln!(writer, "---\n")?;
+        }
+
+        // write title as header with level 1
+
+        write_heading(writer, nodo.metadata().title(), 1)?;
+        writeln!(writer)?;
+        // write fields to the file
+
+        for (i, block) in nodo.blocks().iter().enumerate() {
+            match block {
+                Block::List(items) => write_list(writer, items)?,
+                Block::Heading(t, l) => write_heading(writer, t, *l)?,
+            }
+            if i != nodo.blocks().len() - 1 {
+                writeln!(writer)?;
+            }
+        }
+        Ok(())
+    }
 }
 
-fn read_frontmatter(nodo: &mut Nodo, events_iter: &mut EventsIter) -> Result<(), ReadError> {
+fn read_frontmatter<F: NodoFile>(
+    mut nodo: Nodo<F>,
+    events_iter: &mut EventsIter,
+) -> Result<Nodo<F>, ReadError> {
     let mut in_frontmatter = false;
     for event in events_iter {
         if !in_frontmatter {
             match event {
                 Event::Rule => in_frontmatter = true,
-                _ => return Ok(()),
+                _ => return Ok(nodo),
             }
         } else {
             // in_frontmatter
             match event {
-                Event::Rule | Event::End(Tag::Heading(_)) => return Ok(()),
+                Event::Rule | Event::End(Tag::Heading(_)) => return Ok(nodo),
                 Event::Start(Tag::Heading(_)) | Event::SoftBreak | Event::HardBreak => continue,
                 Event::Text(text) => {
                     let text = text.trim();
-                    if text.starts_with("project:") {
-                        nodo.project(text.trim_start_matches("project:").trim());
-                    } else if text.starts_with("tags:") {
-                        nodo.tags(
+                    if text.starts_with("tags:") {
+                        nodo = nodo.tags(
                             &text
                                 .trim_start_matches("tags:")
                                 .split(',')
@@ -82,22 +121,23 @@ fn read_frontmatter(nodo: &mut Nodo, events_iter: &mut EventsIter) -> Result<(),
             }
         }
     }
-    Ok(())
+    Ok(nodo)
 }
 
-fn read_body(nodo: &mut Nodo, mut events_iter: &mut EventsIter) -> Result<(), ReadError> {
+fn read_body<F: NodoFile>(
+    mut nodo: Nodo<F>,
+    mut events_iter: &mut EventsIter,
+) -> Result<Nodo<F>, ReadError> {
     while let Some(event) = events_iter.next() {
         match event {
             Event::Start(Tag::Heading(level)) => {
-                nodo.heading(read_heading(&mut events_iter), level);
+                nodo = nodo.heading(read_heading(&mut events_iter), level);
             }
-            Event::Start(Tag::List(_first_index)) => {
-                nodo.list(&read_list(&mut events_iter));
-            }
+            Event::Start(Tag::List(_first_index)) => nodo = nodo.list(read_list(&mut events_iter)),
             _ => continue,
         }
     }
-    Ok(())
+    Ok(nodo)
 }
 
 fn read_heading(events_iter: &mut EventsIter) -> String {
@@ -110,45 +150,65 @@ fn read_heading(events_iter: &mut EventsIter) -> String {
     String::new()
 }
 
-fn read_list(mut events_iter: &mut EventsIter) -> Vec<String> {
+fn read_list(mut events_iter: &mut EventsIter) -> Vec<ListItem> {
     let mut items = Vec::new();
     while let Some(event) = events_iter.next() {
         match event {
             Event::Start(Tag::Item) => items.push(read_list_item(&mut events_iter)),
+            Event::End(Tag::List(_first_index)) => return items,
             _ => continue,
         }
     }
     items
 }
 
-fn read_list_item(events_iter: &mut EventsIter) -> String {
+fn read_list_item(events_iter: &mut EventsIter) -> ListItem {
+    let mut text = String::new();
+    let mut is_task = false;
+    let mut checked = false;
     for event in events_iter {
         match event {
-            Event::Text(t) => return t.into_string(),
+            Event::Text(t) => text += &t.into_string(),
+            Event::End(Tag::Item) => {
+                if is_task {
+                    return ListItem::Task(Task::new(text, checked));
+                } else {
+                    return ListItem::Text(text);
+                }
+            }
+            Event::TaskListMarker(ticked) => {
+                is_task = true;
+                checked = ticked;
+            }
             _ => continue,
         }
     }
-    String::new()
+    ListItem::Text(String::new())
 }
 
-impl NodoWriter for Markdown {
-    fn write<W: std::io::Write>(nodo: &Nodo, mut writer: W) -> Result<(), WriteError> {
-        let mut write_line = |s: &str| match writeln!(writer, "{}", s) {
-            Err(ioerr) => Err(WriteError::IOError(ioerr)),
-            Ok(_) => Ok(()),
-        };
-        let mut metadata_lines = Vec::new();
-        metadata_lines.push(nodo.metadata().tags().join(", "));
-        let metadata_str = metadata_lines.join("\n");
-        if metadata_str != "" {
-            write_line("---")?;
-            write_line(&metadata_str)?;
-            write_line("---")?;
-        }
-        Ok(())
+fn write_heading<W: Write>(writer: &mut W, text: &str, level: u32) -> Result<(), WriteError> {
+    writeln!(
+        writer,
+        "{}",
+        &format!("{} {}", "#".repeat(level as usize), text)
+    )?;
+    Ok(())
+}
 
-        // write fields to the file
+fn write_list<W: Write>(writer: &mut W, list_items: &[ListItem]) -> Result<(), WriteError> {
+    for item in list_items {
+        match item {
+            ListItem::Text(s) => writeln!(writer, "{}", &format!("- {}", s))?,
+            ListItem::Task(task) => {
+                if task.checked {
+                    writeln!(writer, "- [x] {}", task.text)?
+                } else {
+                    writeln!(writer, "- [ ] {}", task.text)?
+                }
+            }
+        }
     }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -156,20 +216,68 @@ mod test {
     use super::*;
     use pretty_assertions::assert_eq;
 
+    fn get_test_nodo() -> Nodo<Markdown> {
+        Nodo::new()
+            .tags(&[
+                "nodo".to_owned(),
+                "more tags".to_owned(),
+                "hey another tag".to_owned(),
+            ])
+            .title("nodo header level 1, is the title".to_owned())
+            .list(vec![
+                ListItem::Text("list item 1".to_owned()),
+                ListItem::Text("list item 2".to_owned()),
+            ])
+            .heading("nodo header with level 2".to_owned(), 2)
+            .list(vec![
+                ListItem::Task(Task::new("An item to complete".to_string(), false)),
+                ListItem::Task(Task::new("A completed item, yay".to_string(), true)),
+                ListItem::Text("a text list item".to_owned()),
+                ListItem::Task(Task::new("or a task list item".to_string(), false)),
+            ])
+    }
+
+    fn get_test_nodo_string() -> &'static str {
+        "---
+tags: nodo, more tags, hey another tag
+---
+
+# nodo header level 1, is the title
+
+- list item 1
+- list item 2
+
+## nodo header with level 2
+
+- [ ] An item to complete
+- [x] A completed item, yay
+- a text list item
+- [ ] or a task list item
+"
+    }
+
     #[test]
-    fn test_read_md() {
-        let f = std::fs::File::open("test/markdown/nodo1.md");
+    fn test_read() {
         assert_eq!(
-            &Markdown::read(f.unwrap()).unwrap(),
-            Nodo::new()
-                .project("myproject")
-                .tags(&[
-                    "nodo".to_owned(),
-                    "more tags".to_owned(),
-                    "hey another tag".to_owned()
-                ])
-                .heading("nodo header".to_owned(), 1)
-                .list(&["list item 1".to_owned(), "list item 2".to_owned()])
+            Markdown::read(Nodo::new(), &mut get_test_nodo_string().as_bytes()).unwrap(),
+            get_test_nodo()
+        );
+    }
+
+    #[test]
+    fn test_write() {
+        let mut writer: Vec<u8> = Vec::new();
+        Markdown::write(&get_test_nodo(), &mut writer).unwrap();
+        assert_eq!(String::from_utf8(writer).unwrap(), get_test_nodo_string());
+    }
+
+    #[test]
+    fn test_write_read_gives_same_nodo() {
+        let mut s = Vec::new();
+        Markdown::write(&get_test_nodo(), &mut s).unwrap();
+        assert_eq!(
+            Markdown::read(Nodo::new(), &mut &s[..]).unwrap(),
+            get_test_nodo()
         );
     }
 }
--- src/files/mod.rs
+++ src/files/mod.rs
@@ -6,19 +6,62 @@ use crate::nodo::Nodo;
 #[derive(Debug)]
 pub enum ReadError {
     InvalidElement(String),
+    Io(std::io::Error),
 }
+
+impl From<std::io::Error> for ReadError {
+    fn from(err: std::io::Error) -> ReadError {
+        ReadError::Io(err)
+    }
+}
+
+impl std::fmt::Display for ReadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            ReadError::InvalidElement(s) => write!(f, "{}", s),
+            ReadError::Io(ioerr) => write!(f, "{}", ioerr),
+        }
+    }
+}
+
+impl std::error::Error for ReadError {}
 
 #[derive(Debug)]
 pub enum WriteError {
-    IOError(std::io::Error),
+    Io(std::io::Error),
 }
 
-/// A reader for nodos
-pub trait NodoReader {
-    fn read<R: std::io::Read>(r: R) -> Result<Nodo, ReadError>;
+impl From<std::io::Error> for WriteError {
+    fn from(err: std::io::Error) -> WriteError {
+        WriteError::Io(err)
+    }
 }
 
-/// A writer for nodos
-pub trait NodoWriter {
-    fn write<W: std::io::Write>(nodo: &Nodo, w: W) -> Result<(), WriteError>;
+impl std::fmt::Display for WriteError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            WriteError::Io(ioerr) => write!(f, "{}", ioerr),
+        }
+    }
+}
+
+impl std::error::Error for WriteError {}
+
+pub trait NodoFile: std::fmt::Debug {
+    const EXTENSION: &'static str;
+    fn read<R>(nodo: Nodo<Self>, r: &mut R) -> Result<Nodo<Self>, ReadError>
+    where
+        Self: Sized + NodoFile,
+        R: std::io::Read;
+    fn write<W>(nodo: &Nodo<Self>, w: &mut W) -> Result<(), WriteError>
+    where
+        W: std::io::Write,
+        Self: Sized;
+}
+
+pub fn get_nodo(ft: &str) -> Nodo<impl NodoFile> {
+    match ft {
+        markdown::Markdown::EXTENSION => Nodo::<markdown::Markdown>::new(),
+        _ => panic!("Couldn't get file handler"),
+    }
 }
