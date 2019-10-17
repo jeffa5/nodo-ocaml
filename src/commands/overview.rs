4679222dbb745ba3d8643985cc20e43f49d1e2f6
Jeffas <dev@jeffas.io>
2019-10-17 22:31:14 +0100

    Add completion status to overview

--- src/commands/overview.rs
+++ src/commands/overview.rs
@@ -6,17 +6,20 @@ use walkdir::WalkDir;
 use crate::cli::Overview;
 use crate::commands::CommandError;
 use crate::config::Config;
+use crate::files;
+use crate::files::NodoFile;
+use crate::nodo::{Block, List, ListItem, NodoBuilder};
 use crate::util::file::build_path;
 
 impl Overview {
     pub fn exec(self, config: Config) -> Result<(), CommandError> {
         let path = build_path(&config, &self.target.target);
         if self.target.target.is_empty() {
-            project_overview(&path)?;
+            project_overview(&config, &path)?;
         } else {
             let metadata = fs::metadata(&path)?;
             if metadata.is_dir() {
-                project_overview(&path)?;
+                project_overview(&config, &path)?;
             } else if metadata.is_file() {
                 unimplemented!()
             }
@@ -25,7 +28,7 @@ impl Overview {
     }
 }
 
-fn project_overview(base_path: &Path) -> Result<(), CommandError> {
+fn project_overview(config: &Config, base_path: &Path) -> Result<(), CommandError> {
     let mut depth = 0;
     for entry in WalkDir::new(&base_path).min_depth(1) {
         let entry = entry?;
@@ -48,16 +51,45 @@ fn project_overview(base_path: &Path) -> Result<(), CommandError> {
                     .to_string_lossy()
             );
         } else if entry.file_type().is_file() {
+            let (complete, total) = get_num_complete(config, entry.path())?;
+            let complete_string = if total > 0 {
+                format!(" completed {}/{}", complete, total)
+            } else {
+                String::new()
+            };
             println!(
-                "{}Nodo: {}",
+                "{}Nodo: {}{}",
                 "  ".repeat(depth + 1),
                 entry
                     .path()
                     .strip_prefix(&base_path)
                     .unwrap()
-                    .to_string_lossy()
+                    .to_string_lossy(),
+                complete_string
             );
         }
     }
     Ok(())
+}
+
+fn get_num_complete(config: &Config, path: &Path) -> Result<(u32, u32), CommandError> {
+    let handler = files::get_file_handler(config.default_filetype);
+    let nodo = handler.read(NodoBuilder::default(), &mut fs::File::open(&path)?)?;
+    let mut total = 0;
+    let mut complete = 0;
+    for block in nodo.blocks() {
+        match block {
+            Block::List(List::Plain(items)) | Block::List(List::Numbered(items, _)) => {
+                for item in items {
+                    // not recursive so doesn't count sub-tasks
+                    if let ListItem::Task(_, true, _) = item {
+                        complete += 1;
+                    }
+                    total += 1;
+                }
+            }
+            _ => (),
+        }
+    }
+    Ok((complete, total))
 }
