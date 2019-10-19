9c7e3c773c38a309f6792e042af2ae52b6bb437d
Jeffas <dev@jeffas.io>
2019-10-19 21:27:42 +0100

    Remove filetype handling for editor
    Files now have to have an extension on disk which can be used by the
    editor to detect the filetype.

--- src/commands/edit.rs
+++ src/commands/edit.rs
@@ -6,8 +6,6 @@ use std::process::Command as Cmd;
 use crate::cli::Edit;
 use crate::commands::CommandError;
 use crate::config::Config;
-use crate::files;
-use crate::files::NodoFile;
 use crate::util::file;
 
 impl Edit {
@@ -33,8 +31,7 @@ impl Edit {
                         path.to_string_lossy()
                     )));
                 }
-                let handler = files::get_file_handler(config.default_filetype);
-                let mut command = get_editor(handler.ext());
+                let mut command = get_editor();
                 command.arg(path);
                 debug!("Editor command is: {:?}", command);
                 if !cfg!(test) {
@@ -47,17 +44,12 @@ impl Edit {
     }
 }
 
-fn get_editor(ext: &str) -> Cmd {
+fn get_editor() -> Cmd {
     let mut editor = "vi".to_string();
     if let Ok(e) = env::var("VISUAL") {
         editor = e
     } else if let Ok(e) = env::var("EDITOR") {
         editor = e
-    }
-    if ["vi", "vim", "nvim"].iter().any(|&x| editor == x) {
-        let mut cmd = Cmd::new(editor);
-        cmd.arg("-c").arg(format!("set ft={}", ext));
-        return cmd;
     }
     Cmd::new(editor)
 }
