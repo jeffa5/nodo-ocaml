35600e76fb24a0c2e26d92bc1c682dcd86448e5d
Jeffas <dev@jeffas.io>
2019-10-30 20:27:13 +0000

    Fix archive logic

--- src/commands/archive.rs
+++ src/commands/archive.rs
@@ -14,14 +14,14 @@ impl Archive {
         }
         let mut path = self.target.build_path(&config, false);
         if !path.exists() && path.extension().is_none() {
-            path.set_extension(config.default_filetype);
+            path.set_extension(&config.default_filetype);
         }
-        let move_target = config.archive_dir.join(&self.target.inner);
+        let mut move_target = config.archive_dir.join(&self.target.inner);
+        if path.is_file() && move_target.extension().is_none() {
+            move_target.set_extension(&config.default_filetype);
+        }
         debug!("Moving {:?} to {:?}", path, move_target);
-        if path.is_file() {
-            fs::create_dir_all(move_target.parent().unwrap())?;
-            fs::rename(path, move_target)?;
-        } else if path.is_dir() {
+        if path.is_file() || path.is_dir() {
             fs::create_dir_all(move_target.parent().unwrap())?;
             fs::rename(path, move_target)?;
         } else {
