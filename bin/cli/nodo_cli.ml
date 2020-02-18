7c0861f3ac700fd435197b1b94e2dc9cbf86e0e6
Jeffas <dev@jeffas.io>
2020-02-18 19:07:32 +0000

    Update cli

--- bin/cli/nodo_cli.ml
+++ bin/cli/nodo_cli.ml
@@ -1,13 +1,23 @@
 (* module Prefix : Nodo_filesystem.Prefix_type = struct *)
-(* let prefix = ["/home"; "andrew"; ".nodo"] *)
+(*   let prefix = ["/home"; "andrew"; ".nodo"] *)
 (* end *)
 
-module Remote : Nodo_git_filesystem.Remote = struct
-  let remote = "git://github.com/jeffat/nodo-test.git"
+(* module Fs = Nodo_filesystem.Make (Prefix) *)
+(* module Cli = Nodo_cli_lib.Cli (Fs) (Nodo_markdown) *)
+
+module Config : Nodo_git_filesystem.Config = struct
+  let dir = "/home/andrew/.nodo-git"
+
+  (* let remote = "git://github.com/jeffat/nodo-test.git" *)
+  let remote = ""
+
+  let author = "andrew <dev@jeffas.io>"
 end
 
-(* module Fs = Nodo_filesystem.Make (Prefix) *)
-module Git = Nodo_git_filesystem (Remote)
+module Git = Nodo_git_filesystem.Make (Config)
 module Cli = Nodo_cli_lib.Cli (Git) (Nodo_markdown)
 
-let () = Cli.exec ~formats:[] ~storage:[]
+let () =
+  Logs.set_reporter (Logs_fmt.reporter ()) ;
+  (* Logs.set_level (Some Logs.Debug) ; *)
+  Cli.exec ~formats:[] ~storage:[]
